ed104f38f4ddb461cdace1cbd0b703ba1ea498de
tungtran2005 <tungyt2022@gmail.com>
2025-10-04 16:21:04 +0700

    Add: Get all classes in ClassRepository and GetClassListByAccountId in ClassService

---
+++ .vs/CopilotSnapshots/3ACA416CC7DCC24583F637D7D2AAACA4/B65ED75E4672DD4FAFE8C906D2178B08/C485A639E59A3D8D4163619E5B089EC2
@@ -0,0 +1,9 @@
+﻿namespace Phan_mem_diem_danh.Services;
+
+public class ClassService
+{
+    public ClassService(Configuration configuration)
+    {
+        
+    }
+}
--- Phan-mem-diem-danh/Services/ClassService.cs
+++ Phan-mem-diem-danh/Services/ClassService.cs
@@ -1,9 +1,28 @@
-﻿namespace Phan_mem_diem_danh.Services;
+﻿using Phan_mem_diem_danh.Database.Entities;
+
+namespace Phan_mem_diem_danh.Services;
 
 public class ClassService
 {
+    private readonly Configuration _configuration;
     public ClassService(Configuration configuration)
     {
-        
+        _configuration = configuration;
+    }
+    public List<Class> GetClassListByAccountId(int accountId)
+    {
+        var accountClasses = _configuration.AccountClassRepository.List()
+            .Where(ac => ac.AccountId == accountId)
+            .ToList();
+        var classList = new List<Class>();
+        foreach (var accountClass in accountClasses)
+        {
+            var cls = _configuration.ClassRepository.Find(accountClass.ClassId);
+            if (cls != null)
+            {
+                classList.Add(cls);
+            }
+        }
+        return classList;
     }
 }
